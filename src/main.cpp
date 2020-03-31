371e7f65328fbf9b3faf1a4f2b3b5d95980eb074
ZX1171 <sinzana9@gmail.com>
2020-03-31 00:03:36 -0400

    Added serial outputs and pulled up inputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#define FLOOR1PIN 2
+#define FLOOR2PIN 3
+#define FLOOR3PIN 4
+
+uint8_t prev;
 
 void setup() {
-  Serial.begin(1200);
+  Serial.begin(9600);
+  pinMode(FLOOR1PIN, INPUT_PULLUP);
+  pinMode(FLOOR2PIN, INPUT_PULLUP);
+  pinMode(FLOOR3PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  Serial.println("Hello World");
+  if ((digitalRead(FLOOR1PIN) == LOW) && prev != FLOOR1PIN) {
+    Serial.println(1);
+    prev = FLOOR1PIN;
+  }
+  if ((digitalRead(FLOOR2PIN) == LOW) && prev != FLOOR2PIN) {
+    Serial.println(2);
+    prev = FLOOR2PIN;
+  }
+  if ((digitalRead(FLOOR3PIN) == LOW) && prev != FLOOR3PIN) {
+    Serial.println(3);
+    prev = FLOOR3PIN;
+  }
 }
