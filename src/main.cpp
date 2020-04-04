433d64ef7780b06c546cf7ddd52dd7979d7d35cd
ZX1171 <sinzana9@gmail.com>
2020-04-04 17:33:47 -0400

    Updated Arduino Code

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #define FLOOR2PIN 3
 #define FLOOR3PIN 4
 
-uint8_t prev;
+uint8_t call;
 
 void setup() {
   Serial.begin(9600);
@@ -13,16 +13,14 @@ void setup() {
 }
 
 void loop() {
-  if ((digitalRead(FLOOR1PIN) == LOW) && prev != FLOOR1PIN) {
-    Serial.println(1);
-    prev = FLOOR1PIN;
+  if (digitalRead(FLOOR1PIN) == LOW){
+    call = 1;
   }
-  if ((digitalRead(FLOOR2PIN) == LOW) && prev != FLOOR2PIN) {
-    Serial.println(2);
-    prev = FLOOR2PIN;
+  if (digitalRead(FLOOR2PIN) == LOW){
+    call = 2;
   }
-  if ((digitalRead(FLOOR3PIN) == LOW) && prev != FLOOR3PIN) {
-    Serial.println(3);
-    prev = FLOOR3PIN;
+  if (digitalRead(FLOOR3PIN) == LOW){
+    call = 3;
   }
+  Serial.println(call);
 }
